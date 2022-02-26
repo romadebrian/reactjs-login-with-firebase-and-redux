d960387a0164143357006e48ece8f57e0acde748
romadebrian <roma_coll04@yahoo.com>
2022-02-26 13:10:14 +0700

    tempalte structure file

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home</h1>
+        <p>Welcome to example reactJs login with firebase</p>
+        <p>also use react-router and redux </p>
+      </div>
+    );
+  }
+}
