38bbc78da2ade3b7c592661c49665986557e6949
romadebrian <roma_coll04@yahoo.com>
2022-02-27 06:21:22 +0700

    header main page

---
+++ src/modules/main/header/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <span className="logo">
+        <h1>Header</h1>
+      </span>
+      <span className="list-menu">
+        <button>Home</button>
+        <button>Profile</button>
+        <select name="menu" id="user">
+          <option value="login">Login</option>
+          <option value="logout">Logout</option>
+        </select>
+      </span>
+      <div className="login-profile">
+        <button>Login</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
