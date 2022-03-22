8f51edd9de97b83ebef06e42d418dd8d03527eaa
romadebrian <roma_coll04@yahoo.com>
2022-03-22 18:53:02 +0700

    show email based on user login with firebase onAuthStateChanged

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,30 @@
 import React, { Component } from "react";
+import { auth } from "../../config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default class Home extends Component {
+  state = {
+    user: {},
+  };
+
+  componentDidMount() {
+    onAuthStateChanged(auth, (currentUser) => {
+      // console.log(currentUser);
+      this.setState({
+        user: currentUser,
+      });
+    });
+  }
+
   render() {
+    // console.log(this.state.user);
+
     return (
       <div>
         <h1>Home</h1>
         <p>Welcome to example reactJs login with firebase</p>
         <p>also use react-router and redux </p>
+        <p> User Login : {this.state.user?.email}</p>
       </div>
     );
   }
