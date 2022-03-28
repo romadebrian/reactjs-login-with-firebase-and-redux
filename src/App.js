e486fea2b1e2ef7f8221f11c00e17c5904445929
romadebrian <roma_coll04@yahoo.com>
2022-03-28 18:49:32 +0700

    clean code

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { Fragment, Component } from "react"; //rfce
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import React, { Component } from "react"; //rfce
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// import Dashboard from "./pages/dashboard/Dashboard";
-// import Admin from "./pages/admin/Admin";
 import Main from "./modules/main/Main";
-import Home from "./pages/home/Home";
-import Profile from "./pages/profile/Profile";
 import Login from "./modules/login/Login";
 
-// import logo from "./logo.svg";
-// import "./App.css";
-
 export default class App extends Component {
-  // const [statusLogin, setStatusLogin] = useState(false);
   render() {
     return (
       <Router>
-        {/* <Fragment> */}
         <Routes>
           <Route path="*" element={<Main />} />
 
           <Route exact path="/login" element={<Login />} />
         </Routes>
-        {/* </Fragment> */}
       </Router>
     );
   }
