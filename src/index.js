f2e6f26ea838230ffc094a4ff58351e2a66f1cd1
romadebrian <roma_coll04@yahoo.com>
2022-03-31 06:26:20 +0700

    set data user in redux with onAuthStateChanged

--- src/index.js
+++ src/index.js
@@ -6,15 +6,20 @@ import reportWebVitals from "./reportWebVitals";
 
 import { Provider } from "react-redux";
 import { storeRedux } from "./redux/redux";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={storeRedux}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+onAuthStateChanged(auth, (currentUser) => {
+  storeRedux.dispatch({ type: "SET_USER", userData: currentUser });
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={storeRedux}>
+        <App />
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
